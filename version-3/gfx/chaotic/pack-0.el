696cbd0e8670253d9bacf55ba97f521ba176e75f
vidak <vidak@riseup.net>
2022-09-19 18:03:58 +0800

    finishing up for today

--- version-3/gfx/chaotic/pack-0.el
+++ version-3/gfx/chaotic/pack-0.el
@@ -1,4 +1,8 @@
 
+;; ┌─┐┌─┐┌─┐
+;; ├┤ │ ┬│ ┬
+;; └─┘└─┘└─┘
+
 (defconst +egg+
   (list
    '(gfx
@@ -8,6 +12,10 @@
      "o"
      "O")
    '(name "egge")))
+
+;; ┌┐ ┌─┐┌┐ ┬ ┬
+;; ├┴┐├─┤├┴┐└┬┘
+;; └─┘┴ ┴└─┘ ┴
 
 (defconst +babby-chaotic+ 
   (list
@@ -19,4 +27,46 @@
      "_(｡◎｡ _)") ;; left-moving
    '(name "chaotic babby")))
 
+;; ┌─┐┬ ┬┬┬  ┌┬┐
+;; │  ├─┤││   ││
+;; └─┘┴ ┴┴┴─┘─┴┘
 
+(defconst +child-chaotic+
+  (list
+   '(gfx
+     "( 。v ° )"
+     "(。v ° )☆"
+     "☆( 。v °)"
+     "☆( >。v °)>"
+     "<(。v °< )☆")
+   '(name "chaotic child")))
+
+;; ┌┬┐┌─┐┌─┐┌┐┌
+;;  │ ├┤ ├┤ │││
+;;  ┴ └─┘└─┘┘└┘
+
+(defconst +teen-chaotic-0+ 
+  (list
+   '(gfx
+       "(°□°)" ;; forward       
+       "(°□° )" ;; left-facing       
+       "( °□°)" ;; right-facing       
+       "(╯°□°)╯" ;; right-moving       
+       "╰(°□°╰)") ;; left-moving
+   '(name "gekido")))
+
+(defconst +teen-chaotic-1+
+  (list
+   '(gfx
+       " (•̀_•́)" ;; forward       
+       " (•̀_•́ )" ;; left-facing       
+       " ( •̀_•́)" ;; right-facing       
+       " (ง •̀_•́)ง" ;; right-moving       
+       " ლ(•̀_•́ლ)") ;; left-moving
+   '(name "hadena")))
+
+;; ┌─┐┌┬┐┬ ┬┬ ┌┬┐
+;; ├─┤ │││ ││  │ 
+;; ┴ ┴─┴┘└─┘┴─┘┴ 
+
+;; # 0
